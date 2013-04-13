066aef7015049af5a4a4657c8a9bf56f92056da6
Henrik Møller Rasmussen <hmr@agitso.com>
2013-04-13 11:51:35 +0200

    First hello world

---
+++ Packages/Application/Newsfix.Core/Classes/Newsfix/Core/Controller/FrontpageController.php
@@ -0,0 +1,17 @@
+<?php
+namespace Newsfix\Core\Controller;
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class FrontpageController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+
+	/**
+	 * @return string
+	 */
+	public function indexAction() {
+	}
+}
+?>
