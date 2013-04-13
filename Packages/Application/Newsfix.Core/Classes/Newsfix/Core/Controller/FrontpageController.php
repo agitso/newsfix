06caa9bd8ff54bd3b80483d6544b2aa874931a7b
Henrik Møller Rasmussen <hmr@agitso.com>
2013-04-13 17:53:23 +0200

    .

--- Packages/Application/Newsfix.Core/Classes/Newsfix/Core/Controller/FrontpageController.php
+++ Packages/Application/Newsfix.Core/Classes/Newsfix/Core/Controller/FrontpageController.php
@@ -28,14 +28,17 @@ class FrontpageController extends AbstractController {
 		foreach($xml->item as $item) {
 			$article = new \Newsfix\Core\Domain\Model\Article();
 			$article->setTitle($item->title);
+			$article->setAbstract($item->description);
+			$article->setDate(new \DateTime($item->pubDate));
+
+			for($i=0; $i<mt_rand(1,5); $i++) {
+				$article->addContent(new \Newsfix\Core\Domain\Model\Content('', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod'));
+			}
+
 			$articles[] = $article;
 		}
 
 		$this->view->assign('articles', $articles);
-
-
-
-
 	}
 }
 ?>
